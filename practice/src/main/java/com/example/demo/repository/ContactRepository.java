e7419d0688f511edcb98333fa3f4d3bf3d95a56f
shogo-matsumura <s.matsumura.cingroup2012@gmail.com>
2024-05-27 16:52:27 +0900

    お問い合わせフォーム実装3

---
+++ practice/src/main/java/com/example/demo/repository/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entity.Contact;
+
+
+public interface ContactRepository extends CrudRepository<Contact, Long>{
+
+}
